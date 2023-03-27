f9d2bf9f653edd890f79d2bb1addbcf3d7643a3a
Tom <tom@gmail.com>
2023-03-27 08:44:18 -0500

    pepe

---
+++ examples/calc2.php
@@ -0,0 +1,9 @@
+<?php
+//calc V1.0
+//Developer: Pepe_iso
+//step1 get values from html form
+$num1 = $_POST["n1"];
+$num2 = $_POST["n2"];
+$suma=$num1+$num2;
+echo "<center><h3>".$suma."</h3></center>";
+?>
---
+++ examples/welcome.php
@@ -0,0 +1,10 @@
+<?php
+// this is my first php file
+echo "welcome, this is a php file";
+$num1 = 69;
+$num2 = 1;
+$suma = $num1+$num2;
+$my_name = "Joan";
+echo "Hi ".$my_name;
+echo "the answer is: ".$suma;
+?>
